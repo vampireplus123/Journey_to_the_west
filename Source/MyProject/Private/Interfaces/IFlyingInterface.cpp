3d892efb921954b251f6fc5b52fccd4d6a020aa5
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-10-24 14:07:31 +0700

    delete all files

--- Source/MyProject/Private/Interfaces/IFlyingInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interfaces/IFlyingInterface.h"
-
-// Add default functionality here for any IIFlyingInterface functions that are not pure virtual.
--- Source/MyProject/Private/Interfaces/JumpingInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interfaces/JumpingInterface.h"
-
-// Add default functionality here for any IJumpingInterface functions that are not pure virtual.
